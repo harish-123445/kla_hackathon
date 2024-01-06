ff80430a3580b98b53b78d2a734e55b445571b8b
harish-123445 <harish2004hari@gmail.com>
2024-01-06 17:01:38 +0530

    hello

--- level2a.py
+++ level2a.py
@@ -24,3 +24,60 @@ print("capacity of scooter :",scooter_cap)
 
 print("Total quantity :",sum(ord_quantity))
 
+print("minimum number of slots :",sum(ord_quantity)/sum(scooter_cap))
+
+scooter_cap.sort(reverse=True)
+c=0
+current_scooter=scooter_cap[c]
+
+def nearest_neighbor(distance_matrix, ord_quantity):
+    c = 0  # Initialize the variable 'c'
+    unvisited = set(range(len(distance_matrix)))
+    current_node = 0  
+    current_capacity = 0
+    current_distance = 0
+    slots = []
+    current_slot=[]
+    global current_scooter
+    while unvisited:
+        unvisited.remove(current_node)
+        if current_capacity + ord_quantity[current_node] <=current_scooter :
+            current_capacity += ord_quantity[current_node]
+            current_slot.append(current_node)
+        else:
+            slots.append((current_slot, current_distance,c))
+            current_slot = [current_node]
+            current_capacity = ord_quantity[current_node]
+            current_distance = 0
+            c+=1
+            if (c>4):
+                c=0
+            current_scooter=scooter_cap[c]
+
+        if unvisited:
+            next_node = min(unvisited, key=lambda x: distance_matrix[current_node][x])
+            current_distance += distance_matrix[current_node][next_node]
+            current_node = next_node
+
+    if current_slot:
+        slots.append((current_slot, current_distance))
+
+    return slots
+
+slots = nearest_neighbor(distance, ord_quantity)
+print(slots)
+final_lst=[]
+for i in slots:
+    temp=[]
+    temp.append('r0')
+    for j in i[0]:
+        temp.append('n'+str(j))
+    temp.append('r0')
+    final_lst.append(temp)
+print(final_lst)
+
+final_paths={"v0": {"path1": final_lst[0], "path2": final_lst[5]}, "v1": {"path1": final_lst[1], "path2":final_lst[6]}, "v2": {"path1": final_lst[2]},'v3':{"path1": final_lst[3]},'v4':{"path1": final_lst[4]}}
+
+save_file = open("kla_hackathon/my_output/level2a_output.json", "w")  
+json.dump(final_paths, save_file, indent = 6)  
+save_file.close()
